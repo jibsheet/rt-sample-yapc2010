62c4bc2acd1a68e567c0e33f813762a58f8e822a
Kevin Falcone <falcone@bestpractical.com>
2010-06-22 17:48:39 -0400

    Add final versions of the sample module

--- lib/RT/Action/DefaultCustomFields.pm
+++ lib/RT/Action/DefaultCustomFields.pm
@@ -1,2 +1,13 @@
 package RT::Action::DefaultCustomFields;
+use base qw(RT::Action);
+
+sub Prepare { 1 };
+
+sub Commit {
+    my $self = shift;
+    my $T = $self->TicketObj;
+    $T->AddCustomFieldValue(Field => 'Billable', Value => 'no') 
+        unless $T->CheckSomething;
+};
+
 1;
--- lib/RT/Condition/DepartmentChange.pm
+++ lib/RT/Condition/DepartmentChange.pm
@@ -1,3 +1,22 @@
 package RT::Condition::DepartmentChange;
+use base qw(RT::Condition);
+
+sub IsApplicable {
+    my $self = shift;
+    my $T = $self->TransactionObj;
+    my $Department = RT::CustomField->new($RT::SystemUser);
+    my ($ok, $msg) = $Department->Load('Department');
+    unless ($ok) {
+        $RT::Logger->error("Failed to load Department CF: $msg");
+        return 0;
+    }
+
+    if (  $T->Type eq 'Create' || 
+        ( $T->Type eq 'CustomField' && $T->Field == $Department->Id ) ) {
+        return 1;
+    } 
+
+    return 0;
+}
 
 1;
